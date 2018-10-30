4ac4e73c718752389734b5415dc80f6ffbe3611f
WhyMEliu <527453497@qq.com>
2018-10-30 10:56:00 +0800

    m2007Excel analysis

---
+++ Excel解析/新版2007/excelanalysis/NewExcelUtils.java
@@ -0,0 +1,240 @@
+/******************************************************************
+ * NewExcelUtils.java
+ * Copyright 2018 by GNNT Company. All Rights Reserved.
+ * CreateDate：2018年10月30日
+ * Author：Liupeng
+ * Version：F1.0.0.0
+ ******************************************************************/
+
+package excelanalysis;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * <b>修改记录：</b> 
+ * <p>
+ * <li>
+ * 
+ *                        ---- LP 2018年10月30日
+ * </li>
+ * </p>
+ * 
+ * <b>类说明：</b>
+ * <p> 
+ * 
+ * </p>
+ */
+public class NewExcelUtils {
+	  private XSSFWorkbook workbook;
+	  private XSSFSheet sheet;
+
+	  public NewExcelUtils(String excelURI)
+	  {
+	    if ((excelURI == null) || (excelURI.trim().length() <= 0)) {
+	      throw new IllegalArgumentException("传入路径为空");
+	    }
+
+	    InputStream inputStream = null;
+	    try
+	    {
+	      inputStream = new FileInputStream(excelURI);
+
+	      this.workbook = new XSSFWorkbook(inputStream);
+	    } catch (FileNotFoundException e) {
+	    	System.out.println("文件不存在");
+	    } catch (IOException e) {
+	    	System.out.println("读取文件失败");
+	    } finally {
+	      if (inputStream != null)
+	        try {
+	          inputStream.close();
+	        }
+	        catch (Exception localException)
+	        {
+	        }
+	    }
+	  }
+
+	  public NewExcelUtils(InputStream inputStream)
+	  {
+	    try
+	    {
+	      this.workbook = new XSSFWorkbook(inputStream);
+	    } catch (IOException e) {
+	      System.out.println("读取文件失败");
+	    }
+	  }
+
+	  public Object readPoint(int rowNum, int cellNum)
+	  {
+	    int totalRow = getTotalRow();
+
+	    if (rowNum >= totalRow) {
+	      return "";
+	    }
+
+	    XSSFRow rows = getSheet().getRow(rowNum);
+
+	    return getValue(rows.getCell(cellNum));
+	  }
+
+	  public Object[] readCell(int cellNum)
+	  {
+	    int totalRow = getTotalRow();
+
+	    Object[] result = new Object[totalRow];
+
+	    for (int i = 0; i < totalRow; i++) {
+	      result[i] = readPoint(i, cellNum);
+	    }
+	    return result;
+	  }
+
+	  public Object[] readRow(int rowNum)
+	  {
+	    int totalRow = getTotalRow();
+
+	    if (rowNum >= totalRow) {
+	      return new String[0];
+	    }
+
+	    XSSFRow row = getSheet().getRow(rowNum);
+
+	    int cellNum = getSheet().getRow(rowNum).getPhysicalNumberOfCells();
+
+	    Object[] result = new Object[cellNum];
+
+	    for (int i = 0; i < cellNum; i++)
+	    {
+	      result[i] = getValue(row.getCell(i));
+	    }
+	    return result;
+	  }
+
+	  public List<Object[]> readPage(int startRowIndex, int endRowIndex)
+	  {
+	    if (startRowIndex < 0) {
+	      startRowIndex = 0;
+	    }
+
+	    int totalRow = getTotalRow();
+
+	    if ((endRowIndex < 0) || (endRowIndex > totalRow)) {
+	      endRowIndex = totalRow;
+	    }
+
+	    List result = new ArrayList();
+
+	    Object[] cells = null;
+
+	    for (int i = startRowIndex; i < endRowIndex; i++)
+	    {
+	      XSSFRow row = getSheet().getRow(i);
+
+	      if (row == null) {
+	        cells = new Object[0];
+	        result.add(cells);
+	      }
+	      else
+	      {
+	        int cellNum = row.getPhysicalNumberOfCells();
+
+	        cells = new Object[cellNum];
+
+	        for (short j = 0; j < cellNum; j = (short)(j + 1))
+	        {
+	          cells[j] = getValue(row.getCell(j));
+	        }
+	        result.add(cells);
+	      }
+	    }
+	    return result;
+	  }
+
+	  private Object getValue(XSSFCell cell)
+	  {
+	    if (cell == null) {
+	      return "";
+	    }
+
+	    switch (cell.getCellType()) {
+	    case 3:
+	      return "";
+	    case 5:
+	      return "";
+	    case 1:
+	      return cell.getStringCellValue();
+	    case 4:
+	      return Boolean.valueOf(cell.getBooleanCellValue());
+	    case 0:
+	      if (HSSFDateUtil.isCellDateFormatted(cell)) {
+	        return cell.getDateCellValue();
+	      }
+	      return cell.getRawValue();
+	    case 2:
+	      try
+	      {
+	        String value = String.valueOf(cell.getNumericCellValue());
+
+	        if (value.indexOf("E") != -1) {
+	          BigDecimal bd = new BigDecimal(cell.getNumericCellValue());
+	          bd.setScale(2);
+	          value = bd.toString();
+	        }
+	        if (!value.equals("NaN")) break;
+	        value = cell.getStringCellValue().toString();
+	      }
+	      catch (Exception e) {
+	        return cell.getRawValue();
+	      }
+	    }
+	    return cell.getRawValue();
+	  }
+
+	  private XSSFSheet getSheet()
+	  {
+	    if (this.sheet == null) {
+	      synchronized (this) {
+	        if (this.sheet == null) {
+	          this.sheet = this.workbook.getSheetAt(0);
+	        }
+	      }
+	    }
+	    return this.sheet;
+	  }
+
+	  public void setSheetName(String sheetName)
+	  {
+	    XSSFSheet getSheet = this.workbook.getSheet(sheetName);
+	    if (getSheet == null) {
+	      throw new IllegalArgumentException("将要获取的页面[" + sheetName + "]不存在");
+	    }
+	    this.sheet = getSheet;
+	  }
+
+	  public void setSheetIndex(int index)
+	  {
+	    XSSFSheet getSheet = this.workbook.getSheetAt(index);
+	    if (getSheet == null) {
+	      throw new IllegalArgumentException("将要获取的页面下标[" + index + "]不存在");
+	    }
+	    this.sheet = getSheet;
+	  }
+
+	  public int getTotalRow()
+	  {
+	    return getSheet().getPhysicalNumberOfRows();
+	  }
+}
---
+++ Excel解析/新版2007/excelanalysis/TestNewExcel.java
@@ -0,0 +1,87 @@
+/******************************************************************
+ * TestNewExcel.java
+ * Copyright 2018 by GNNT Company. All Rights Reserved.
+ * CreateDate：2018年10月30日
+ * Author：Liupeng
+ * Version：F1.0.0.0
+ ******************************************************************/
+
+package excelanalysis;
+
+import java.io.File;
+import java.util.List;
+
+
+/**
+ * <b>修改记录：</b> 
+ * <p>
+ * <li>
+ * 
+ *                        ---- LP 2018年10月30日
+ * </li>
+ * </p>
+ * 
+ * <b>类说明：</b>
+ * <p> 
+ * 
+ * </p>
+ */
+public class TestNewExcel {
+	public static void main(String[] args) {
+		
+		NewExcelUtils excelReader = new NewExcelUtils("C:\\Users\\jw\\Desktop\\ttli\\newjiex.xlsx");
+		//获取上传文件一共有多少行
+		int objTotal = excelReader.getTotalRow();
+		System.out.println("objTotal:"+objTotal);
+		
+		//接收所有数据
+		List<Object[]> allData = excelReader.readPage(0, objTotal);
+		System.out.println("allData:"+allData);
+
+		//判断是否有空行，0表示无空行
+		int isData=rowIsData(allData);
+		if(isData==0&&objTotal!=1){
+			for (int i = 1; i < objTotal; i++) {
+				Object[] o=allData.get(i);
+				System.out.println(o[0].toString());
+				System.out.println(o[1].toString());
+			}
+		}
+	}
+		
+	/**
+	 * 
+	 * <b>方法说明：</b>
+	 * <ul>
+	 * 判断是否有空行，如果有则返回对应空行的所在行数
+	 * </ul>
+	 * @param params
+	 * @return
+	 */
+	private static int rowIsData(List<Object[]> objPage){
+		int flag=0;
+		boolean exist=false;
+		if(objPage.size()>0){
+			//遍历objPage集合
+			for(int i=0;i<objPage.size();i++){
+				exist=false;
+				Object[] params=objPage.get(i);
+				//遍历每行的每列
+				for(int j=0;j<params.length;j++){
+					String param=params[j]+"";
+					//只有要一列有数据说明该行不是空行
+					if(!"".equals(param.trim())){
+						exist=true;
+						break;
+					}
+				}
+				//有空行返回空行所在行数
+				if(!exist){
+					flag=i;
+					return flag;
+				}
+			}
+		}
+		return flag;
+	}
+}
